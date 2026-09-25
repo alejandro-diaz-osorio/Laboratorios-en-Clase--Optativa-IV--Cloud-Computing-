36e09b9be8aff8bfd162905e89f049ce2d251510
alejandro-diaz-osorio <alejandro.diaz@ucp.edu.co>
2026-09-24 23:09:46 -0500

    Parte Final del Taller No. 1

--- 24-09/Taller #1/server.js
+++ 24-09/Taller #1/server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 app.get('/', (req, res) => {
-    res.send('<h1>🚀 ¡Reto Docker Completado Exitosamente por Estudiante!</h1>');
+    res.send('<h1>🚀 ¡Reto Docker Completado Exitosamente por Alejandro Díaz Osorio!</h1>');
 });
 app.listen(PORT, () => console.log('Corriendo en puerto ' + PORT));
