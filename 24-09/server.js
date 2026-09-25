46c8bac58d8ab766be06f9c688a2034ee4dc5e9b
alejandro-diaz-osorio <alejandro.diaz@ucp.edu.co>
2026-09-24 19:55:53 -0500

    24-09: Creación del primer proyecto con Docker

---
+++ 24-09/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>¡Hola Mundo desde mi contenedor de Docker! 🚀</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
